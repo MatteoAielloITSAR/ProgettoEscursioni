f2926d94568bf3a7a02b5ee673a5b4d5e9334e07
g-giacobbe <g.giacobbe@itsrizzoli.it>
2021-01-21 21:52:57 +0100

    connessione database tramite jpa

---
+++ connessione-db/src/main/java/main/App.java
@@ -0,0 +1,13 @@
+package main;
+
+import util.JpaUtilSingleton;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+	var em = JpaUtilSingleton.getInstance().getEntityManager();
+
+	}
+
+}
