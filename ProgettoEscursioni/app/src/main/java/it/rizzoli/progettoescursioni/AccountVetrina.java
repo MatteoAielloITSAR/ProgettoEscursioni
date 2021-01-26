bd3812cbd5872032f2f0c07a3d728e76c168acbe
StefanoGalata <s.galata@itsrizzoli.it>
2021-01-26 16:39:36 +0100

    aggiornamento activity android

---
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/AccountVetrina.java
@@ -0,0 +1,14 @@
+package it.rizzoli.progettoescursioni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountVetrina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_vetrina);
+    }
+}
