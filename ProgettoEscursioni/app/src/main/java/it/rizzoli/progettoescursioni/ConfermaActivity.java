502a40ecf363058863005f6aa4913165acaf5ee4
StefanoGalata <s.galata@itsrizzoli.it>
2021-01-26 15:21:29 +0100

    Aggiunto codice conferma android

---
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/ConfermaActivity.java
@@ -0,0 +1,30 @@
+package it.rizzoli.progettoescursioni;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfermaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recuperopassword);
+
+        EditText etCode = findViewById(R.id.etCode);
+        TextView twCode = findViewById(R.id.twCode);
+        Button btnCodeconfirm = findViewById(R.id.btnCodeconfirm);
+
+        btnCodeconfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ConfermaActivity.this, "btnCodeconfirm", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
