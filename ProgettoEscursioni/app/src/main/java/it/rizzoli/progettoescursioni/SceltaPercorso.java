8857852553a0d80a4e675011ca929d874f88d154
StefanoGalata <s.galata@itsrizzoli.it>
2021-01-26 17:08:39 +0100

    aggiunto scelta percorso android

--- ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/SceltaPercorso.java
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/SceltaPercorso.java
@@ -3,6 +3,9 @@ package it.rizzoli.progettoescursioni;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SceltaPercorso extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class SceltaPercorso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scelta_percorso);
+        Button btnRegistrazione = findViewById(R.id.btnRegistrazione);
+        Button btnManuale = findViewById(R.id.btnManuale);
+
+        btnRegistrazione.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SceltaPercorso.this, "btnRegistrazione", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnManuale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SceltaPercorso.this, "btnManuale", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
