7a9b2bc073ed87f4c90c056dd3ed4f91b5c9cb44
g-giacobbe <g.giacobbe@itsrizzoli.it>
2021-01-29 17:32:25 +0100

    -----

--- progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/model/Immagine.java
+++ progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/model/Immagine.java
@@ -1,9 +1,12 @@
 package it.rizzoli.ifts.progettoescursioni01.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Immagine {
+	@Id
+	private Integer idImmagine;
 	//aggiungere percorso_file(?)
 
 }
