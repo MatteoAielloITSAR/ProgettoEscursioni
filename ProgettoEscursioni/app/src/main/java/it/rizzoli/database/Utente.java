b90b06b840c79c92de5e9ff8a0a2bc47f4d12073
AzureAD\SamueleFattoretti <s.fattoretti@itsrizzoli.it>
2021-02-09 17:44:34 +0100

    no message

---
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/database/Utente.java
@@ -0,0 +1,54 @@
+package it.rizzoli.database;
+
+public class Utente {
+    private int id;
+    private String nome;
+    private String cognome;
+    private String username;
+    private String password;
+
+    public Utente(String user){
+        username=user;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/IscrizioniActivity.java
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/IscrizioniActivity.java
@@ -3,6 +3,14 @@ package it.rizzoli.progettoescursioni;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import it.rizzoli.database.Utente;
 
 public class IscrizioniActivity extends AppCompatActivity {
 
@@ -10,5 +18,23 @@ public class IscrizioniActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iscrizioni);
+
+        ArrayList<Utente> list=new ArrayList<>();
+        list.add(new Utente("pippo"));
+        list.add(new Utente("pluto"));
+
+        IscrizioniListAdapter iscrizioniListAdapter=new IscrizioniListAdapter(this,R.layout.list_account,list);
+        ListView iscrizioniListView = findViewById(R.id.iscrizioniListView);
+        iscrizioniListView.setAdapter(iscrizioniListAdapter);
+
+        iscrizioniListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Utente ut = iscrizioniListAdapter.getItem(i);
+                String user = ut.getUsername() ;
+                Toast.makeText(IscrizioniActivity.this, user, Toast.LENGTH_LONG).show();
+            }
+        });
+  
     }
 }
