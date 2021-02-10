2b6c1cd0fa29c81cf8bd35cce6bb8dfc3037c754
g-giacobbe <g.giacobbe@itsrizzoli.it>
2021-02-10 10:29:06 +0100

    aggiunti intent a post

--- ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/PostActivity.java
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/PostActivity.java
@@ -2,7 +2,10 @@ package it.rizzoli.progettoescursioni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        Button btnUsername=findViewById(R.id.usernameAccount);
+        btnUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent postIntent = new Intent(PostActivity.this, AccountVetrina.class);
+                startActivity(postIntent);
+            }
+        });
+
+        /*Button btnVisualMapPer=findViewById(R.id.visualizzaMappaPercorso);
+        btnVisualMapPer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent postIntent = new Intent(PostActivity.this, AccountVetrina.class);
+               // startActivity(postIntent);
+            }
+        });
+
+         */
     }
 }
