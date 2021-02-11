bc4006a24adcb66f9f713b832cf2bd6325ad2b84
AzureAD\SamueleFattoretti <s.fattoretti@itsrizzoli.it>
2021-02-11 17:43:48 +0100

    no message

--- ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/PostIscrizioniActivity.java
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/PostIscrizioniActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -38,6 +40,20 @@ public class PostIscrizioniActivity extends AppCompatActivity {
         PostListAdapter postListAdapter=new PostListAdapter(this,R.layout.list_post,list);
         ListView postListView = findViewById(R.id.percorsiListView);
         postListView.setAdapter(postListAdapter);
+
+        postListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Post p= postListAdapter.getItem(position);
+                String nome=p.getNomePercorso();
+                Toast.makeText(PostIscrizioniActivity.this,nome,Toast.LENGTH_LONG).show();
+
+                Intent postIntent = new Intent(PostIscrizioniActivity.this, PostActivity.class);
+                postIntent.putExtra("POST", p.getId_Post());
+                startActivity(postIntent);
+
+            }
+        });
 
 
     }
