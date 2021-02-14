0bcadfcf0ba36e7bfa2058ca0f9513f35a7d8f1b
AzureAD\MatteoAiello <m.aiello@gmail.com>
2021-02-14 17:29:35 +0100

    aggiunta intent per "minipost"

---
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/IscrizioniListAdapter.java
@@ -0,0 +1,46 @@
+package it.rizzoli.progettoescursioni;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+import it.rizzoli.database.Utente;
+
+public class IscrizioniListAdapter extends ArrayAdapter<Utente> {
+    Context ctx;
+    int res;
+
+    public IscrizioniListAdapter(@NonNull Context context, int resource, ArrayList<Utente> iscrizioni) {
+        super(context, resource);
+
+        addAll(iscrizioni);
+
+        ctx=context;
+        res=resource;
+    }
+
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(res, parent, false);
+
+        TextView usernameTextView = rowView.findViewById(R.id.usenametw);
+
+
+        Utente u = getItem(position);
+
+
+        usernameTextView.setText(u.getNome());
+
+
+        return rowView;
+
+    }
+}
