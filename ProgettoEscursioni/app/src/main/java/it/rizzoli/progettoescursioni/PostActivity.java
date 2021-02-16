fb4492bfed04f3f7498ab8ac822418036de65e58
StefanoGalata <s.galata@itsrizzoli.it>
2021-02-16 16:24:57 +0100

    intent account

--- ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/PostActivity.java
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/PostActivity.java
@@ -24,8 +24,8 @@ public class PostActivity extends AppCompatActivity {
                 startActivity(postIntent);
             }
         });
-
-        Bundle bundle = getIntent().getExtras();
+        Intent i = getIntent();
+        Bundle bundle = i.getExtras();
         String stringUsername = bundle.getString("username");
         String stringNomePercorso = bundle.getString("nome percorso");
         String stringDescrizione = bundle.getString("descrizione");
