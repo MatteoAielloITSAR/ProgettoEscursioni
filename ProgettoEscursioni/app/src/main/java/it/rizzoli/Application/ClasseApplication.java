292a5b84fa48150fa562ce8bd9261cd5576a5f74
AzureAD\MatteoAiello <m.aiello@gmail.com>
2021-02-17 15:52:27 +0100

    creazione calsse application

---
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/Application/ClasseApplication.java
@@ -0,0 +1,13 @@
+package it.rizzoli.Application;
+
+import android.app.Application;
+
+import it.rizzoli.model.Utente;
+
+public class ClasseApplication extends Application {
+    private Utente utente = new Utente();
+
+
+
+
+}
