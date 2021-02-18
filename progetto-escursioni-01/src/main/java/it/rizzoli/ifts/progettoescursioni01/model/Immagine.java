1f5c1c1aeee0029c871c9171dde5a014aa3e4f20
AzureAD\SamueleFattoretti <s.fattoretti@itsrizzoli.it>
2021-02-18 17:57:01 +0100

    no message

--- progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/model/Immagine.java
+++ progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/model/Immagine.java
@@ -1,12 +1,21 @@
 package it.rizzoli.ifts.progettoescursioni01.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Immagine {
 	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
 	private Integer idImmagine;
 	//aggiungere percorso_file(?)
+	
+	private String percorsoFile;
+	
+	@ManyToOne
+	private Post post;
 
 }
