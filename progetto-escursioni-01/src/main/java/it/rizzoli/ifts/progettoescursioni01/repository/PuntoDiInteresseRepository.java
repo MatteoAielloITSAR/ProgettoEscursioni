88ef08068a728829025a56e31610485bb634d3bd
AzureAD\SamueleFattoretti <s.fattoretti@itsrizzoli.it>
2021-03-01 16:54:12 +0100

    no message

--- progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/repository/PuntoDiInteresseRepository.java
+++
@@ -1,9 +0,0 @@
-package it.rizzoli.ifts.progettoescursioni01.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.rizzoli.ifts.progettoescursioni01.model.PuntoDiInteresse;
-
-public interface PuntoDiInteresseRepository extends JpaRepository<PuntoDiInteresse, Integer> {
-
-}
