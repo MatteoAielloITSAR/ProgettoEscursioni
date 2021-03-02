771d9aee7841db6017d20500699a1a9dc11bca68
g-giacobbe <g.giacobbe@itsrizzoli.it>
2021-03-02 15:46:33 +0100

    immagine controller eclipse

--- progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/controller/ImmagineController.java
+++ progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/controller/ImmagineController.java
@@ -3,8 +3,12 @@ package it.rizzoli.ifts.progettoescursioni01.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.rizzoli.ifts.progettoescursioni01.model.Immagine;
@@ -27,5 +31,27 @@ public class ImmagineController {
 	public Immagine byImmagine(@PathVariable Integer idImmagine) {
 		return repository.findById(idImmagine).orElseThrow();
 	}
+	
+	@PostMapping("/immagini")
+	public Immagine inserisci(@RequestBody Immagine immagine) {
+		return repository.save(immagine);
+		}
+	
+	@PutMapping("/immagini/{idImmagine}")
+	public Immagine aggiorna(@RequestBody Immagine immagine, @PathVariable Integer idImmagine) {
+		repository.findById(idImmagine).ifPresentOrElse((i) -> {
+			i.setPercorsoFile(immagine.getPercorsoFile());
+			repository.save(i);
+		}, () -> {
+			repository.save(immagine);
+			});
+		return repository.findById(idImmagine).get();
+	}
+	
+	@DeleteMapping("/immagini/{idImmagine}")
+	public void elimina(@PathVariable Integer idImmagine) {
+			repository.deleteById(idImmagine);
+		}
+	}
 
-}
+
