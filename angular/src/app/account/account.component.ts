04ba2de95444bb7b8b587fe0df2d8e3429893eef
AzureAD\MatteoAiello <m.aiello@gmail.com>
2021-03-09 10:40:55 +0100

    inizio account

--- angular/src/app/account/account.component.ts
+++ angular/src/app/account/account.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Utente } from '../model/utente';
 import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { Post } from '../model/Post';
 
 @Component({
   selector: 'app-account',
@@ -10,29 +11,36 @@ import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 })
 export class AccountComponent implements OnInit {
 
-  utenti : Utente[];
   utente : Utente;
+  posts : Post[];
 
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit(): void {
+    this.utente = JSON.parse(localStorage.getItem('utente'));
+    this.http.get<any[]>('http://localhost:8080/utenti/').subscribe((dati) => {
+      // questa funzione viene richiamata dall'http client quando
+
+
+    /*
     this.utente = new Utente();
     this.http.get<any[]>('http://localhost:8080/utenti').subscribe((dati) => {
       this.utenti = dati;  
       console.log(this.utenti.length)
       for(var i = 0; i < this.utenti.length; i++) {
         console.log(this.utenti[i]);
-        /*
+        
         if (this.utenti[i].nome == 'nome1') {
-          this.utente = this.utenti[i]
+          this.utente = this.utenti[i];
         }
-        */
+        
        
       }
       
     });
+    */
   }
 
   getUtenti() {
--- progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/controller/PostController.java
+++ progetto-escursioni-01/src/main/java/it/rizzoli/ifts/progettoescursioni01/controller/PostController.java
@@ -3,6 +3,7 @@ package it.rizzoli.ifts.progettoescursioni01.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import it.rizzoli.ifts.progettoescursioni01.model.Post;
 import it.rizzoli.ifts.progettoescursioni01.repository.PostRepository;
 import it.rizzoli.ifts.progettoescursioni01.repository.PostRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PostController {
 	@Autowired
