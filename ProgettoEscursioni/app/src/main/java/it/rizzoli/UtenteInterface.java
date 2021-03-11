418287fd7c45bcb85da58ff9f362a822a7f209eb
CHrisFimpa <76052726+CHrisFimpa@users.noreply.github.com>
2021-03-11 11:39:46 +0100

    Retrofit e UtenteInterface

---
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/UtenteInterface.java
@@ -0,0 +1,32 @@
+package it.rizzoli;
+
+import java.util.List;
+
+import it.rizzoli.model.Utente;
+import it.rizzoli.model.Post;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface UtenteInterface {
+
+    @GET()
+    Call<List<Utente>> all();
+
+    @GET("/utenti/{id}")
+    Call<Utente> byId(@Path("id") Integer id);
+
+    @POST("/utente")
+    Call<Utente> inserisci(@Body Utente utente);
+
+    @POST("/utenti/{id}")
+    Call<Utente> inserisciPost(@Body Post post, @Path("id") Integer id);
+
+    @PUT("/utenti/{id}")
+    Call<Utente> aggiorna(@Body Utente utente, @Path("id") Integer id);
+
+
+}
---
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/RetrofitClientInstance.java
@@ -0,0 +1,13 @@
+package it.rizzoli.progettoescursioni;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+
+    public static final String BASE_URL = "";
+    public static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
