56cfd8fa3ff174408d950714a7d84e07419f103f
g-giacobbe <g.giacobbe@itsrizzoli.it>
2021-03-11 16:39:05 +0100

    ---

--- ProgettoEscursioni/app/src/main/java/it/rizzoli/retrofit/RetrofitClientInstance.java
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/retrofit/RetrofitClientInstance.java
@@ -1,13 +1,18 @@
 package it.rizzoli.retrofit;
 
+import android.app.Application;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class RetrofitClientInstance {
+public class RetrofitClientInstance extends Application {
 
     public static final String BASE_URL = "";
     public static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build();
+    }
+
+
 }
