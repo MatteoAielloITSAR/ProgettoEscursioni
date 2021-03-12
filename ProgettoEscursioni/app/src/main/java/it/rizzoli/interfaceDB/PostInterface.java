e6ce83c5e2ac805e61a79b30ea001260f87b7944
g-giacobbe <g.giacobbe@itsrizzoli.it>
2021-03-12 16:17:39 +0100

    postInterface

---
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/interfaceDB/PostInterface.java
@@ -0,0 +1,31 @@
+package it.rizzoli.interfaceDB;
+import java.util.List;
+
+import it.rizzoli.model.Utente;
+import it.rizzoli.model.Post;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface PostInterface {
+
+    @GET()
+    Call<List<Post>> all();
+
+    @GET("/post/{id}")
+    Call<Utente> byId(@Path("id") Integer id);
+
+    @POST("/post")
+    Call<Post> inserisci(@Body Utente utente);
+
+    @POST("/post/{id}")
+    Call<Utente> inserisciPost(@Body Post post, @Path("id") Integer id);
+
+    @PUT("/post/{id}")
+    Call<Utente> aggiorna(@Body Utente utente, @Path("id") Integer id);
+
+
+}
