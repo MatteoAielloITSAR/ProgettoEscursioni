b1d314b010cf8bd35020e60583bac857bd64546a
g-giacobbe <g.giacobbe@itsrizzoli.it>
2021-03-12 18:15:30 +0100

    Iscrizioni Db

--- ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/IscrizioniActivity.java
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/IscrizioniActivity.java
@@ -10,9 +10,13 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import it.rizzoli.interfaceDB.UtenteInterface;
 import it.rizzoli.listadapter.IscrizioniListAdapter;
 import it.rizzoli.model.Utente;
+import it.rizzoli.retrofit.RetrofitClientInstance;
+import retrofit2.Call;
 
 public class IscrizioniActivity extends AppCompatActivity {
 
@@ -39,6 +43,9 @@ public class IscrizioniActivity extends AppCompatActivity {
                 Intent accountVetrinaIntent = new Intent(IscrizioniActivity.this, AccountVetrina.class);
                 accountVetrinaIntent.putExtra("UTENTE", ut.getId());
                 startActivity(accountVetrinaIntent );
+
+                UtenteInterface ui = (new RetrofitClientInstance()).getUtenteInterface();
+                Call<List<Utente>> call = ui.all();
             }
         });
   
