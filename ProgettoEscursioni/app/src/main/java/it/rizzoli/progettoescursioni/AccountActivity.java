25bd5a90d649c5681d3ec6a548b9e38c9caaf66e
AzureAD\MatteoAiello <m.aiello@gmail.com>
2021-03-19 17:44:31 +0100

    creazione menu

--- ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/AccountActivity.java
+++ ProgettoEscursioni/app/src/main/java/it/rizzoli/progettoescursioni/AccountActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,6 +21,40 @@ import it.rizzoli.model.Utente;
 import it.rizzoli.model.UtentePost;
 
 public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.account:
+                Intent accountActivity = new Intent(AccountActivity.this, AccountActivity.class);
+                startActivity(accountActivity);
+                break;
+            case R.id.crea:
+                Intent creaPercorsoActivity = new Intent(AccountActivity.this, CreazionePostActivity.class);
+                startActivity(creaPercorsoActivity);
+                break;
+            case R.id.iscrizioni:
+                Intent iscrizioniActivity = new Intent(AccountActivity.this, PostIscrizioniActivity.class);
+                startActivity(iscrizioniActivity);
+                break;
+            case R.id.ricerca:
+                Intent ricercaActivity = new Intent(AccountActivity.this, SearchPostActivity.class);
+                startActivity(ricercaActivity);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
